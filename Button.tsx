eb534e7ff76c9246ebe90a19df5dd778d1a2db9a
e.shilova <e.shilova@linksat.ru>
2023-04-18 11:32:18 +0300

    Fix prop logic and grammar

--- Button.tsx
+++ Button.tsx
@@ -3,29 +3,29 @@ import { useState } from 'react';
 type Props = {
     text: string,
     backgroundColor: string,
-    callbaсk: () => void,
+    callback: () => void,
 
 }
 
 function Button(props: Props) {
     const [text, setText] = useState<string>(props.text)
-    const [backgroundColor, setBackgroundColor] = useState<string>(props.text)
+    const [backgroundColor, setBackgroundColor] = useState<string>(props.backgroundColor)
 
     const onClick = () => {
         
         setText(preVvalue => {
             if (preVvalue === props.text ){
-                return 'red';
-            } else return props.text
-        });
-
-        setBackgroundColor(preVvalue => {
-            if (preVvalue === props.text ){
                 return 'newText';
             } else return props.text
         });
 
-        props.callbaсk();
+        setBackgroundColor(preVvalue => {
+            if (preVvalue === props.backgroundColor ){
+                return 'red';
+            } else return props.backgroundColor
+        });
+
+        props.callback();
     }; 
 
     return (
